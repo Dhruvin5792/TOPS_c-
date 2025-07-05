9bfa3fc958119e7a245961900ac4676c694e899b
Dhruvin5792 <dhruvin7640virani@gmail.com>
2025-07-05 11:02:39 +0530

    initial commit

---
+++ practical/rectangle.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+main(){
+	int length,width,sum;
+	
+	cout<<"\n Entre a length:-";
+	cin>>length;
+	
+	cout<<"\n Enter a width:-";
+	cin>>width;
+	
+	sum=length*width;
+	cout<<"\n Area Of Rectangle="<<sum;
+	
+}
---
+++ practical/sumofvalue.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+main(){
+	int num1,num2,sum;
+	
+	cout<<"\n Enter A Value Of Number 1:-";
+	cin>>num1;
+	
+	cout<<"\n Enter A Value Of Number 2:-";
+	cin>>num2;
+	
+	sum=num1+num2;
+	cout<<"\n Sum of num1 and num2:-"<<sum;
+
+	
+	
+}
